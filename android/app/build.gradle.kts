46482f7abfab426d4c7ce4b7a8feda8ace13d041
Haruhiko Takada <haruhiko.takada@gmail.com>
2026-03-01 00:24:05 +0900

    Android アプリの名前空間とアプリケーションIDを更新

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.no_zan_lane"
+    namespace = "com.tfandkusu.nozanlane"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.no_zan_lane"
+        applicationId = "com.tfandkusu.nozanlane"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/nozanlane/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nozanlane/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.no_zan_lane
+package com.example.nozanlane
 
 import io.flutter.embedding.android.FlutterActivity
 
