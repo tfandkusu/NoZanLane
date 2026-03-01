cdb016f315796713191c7c716c7f452681e226f0
Haruhiko Takada <haruhiko.takada@gmail.com>
2026-03-01 23:29:31 +0900

    ProviderScopeを追加してWidgetbookAppをラップするようにmain関数を更新

--- lib/widgetbook/main.dart
+++ lib/widgetbook/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:no_zan_lane/widgetbook/app.dart';
 
 void main() {
-  runApp(const WidgetbookApp());
+  runApp(const ProviderScope(child: WidgetbookApp()));
 }
