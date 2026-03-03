b0eb3d127cd4e96cf57416e7059c0b6d8e21dfe1
Haruhiko Takada <haruhiko.takada@gmail.com>
2026-03-03 23:53:17 +0900

    lib/seed 配下と関連参照を削除

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:no_zan_lane/app/app.dart';
-import 'package:no_zan_lane/seed/seed_issue_loader.dart';
-import 'package:no_zan_lane/seed/seed_issue_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final seedIssues = await SeedIssueLoader(
-    assetBundle: rootBundle,
-  ).load();
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
@@ -21,10 +16,5 @@ Future<void> main() async {
       systemNavigationBarContrastEnforced: true,
     ),
   );
-  runApp(
-    ProviderScope(
-      overrides: [seedIssueListProvider.overrideWithValue(seedIssues)],
-      child: const App(),
-    ),
-  );
+  runApp(const ProviderScope(child: App()));
 }
